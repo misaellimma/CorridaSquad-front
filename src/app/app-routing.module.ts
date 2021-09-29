9d9dad5a090d18a1e96072fe55a4aa0e2b1358df
Diogo L. Menezes <dmenezes@itelinfo.com>
2021-09-29 10:28:49 -0400

    Modificando rota de auxiliarMecanico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ import { ServicoAlterarComponent } from './components/servico-alterar/servico-al
 import { ServicoComponent } from './components/servico/servico.component';
 import { ViewCorridaEquipeComponent } from './components/view-corrida-equipe/view-corrida-equipe.component';
 import { ViewMecanicoAuxComponent } from './components/view-mecanico-aux/view-mecanico-aux.component';
+import { ViewMecanicoAuxiliarComponent } from './components/view-mecanico-auxiliar/view-mecanico-auxiliar.component';
 import { ViewQuantidadeCarroCorridaComponent } from './components/view-quantidade-carro-corrida/view-quantidade-carro-corrida.component';
 import { CorridaService } from './services/corrida.service';
 
@@ -40,7 +41,7 @@ const routes: Routes = [
   { path: "equipe", component: EquipeComponent},
   { path: "equipe/alterar/:id", component: EquipeAlterarComponent},
   { path: "viewCorridaEquipe", component: ViewCorridaEquipeComponent},
-  {path:"mecanico/auxiliares", component: ViewMecanicoAuxComponent},
+  {path:"mecanico/auxiliares", component: ViewMecanicoAuxiliarComponent},
   {path:"quantidadeCarroCorrida", component: ViewQuantidadeCarroCorridaComponent}
 ];
 
--- src/app/components/view-mecanico-auxiliar/view-mecanico-auxiliar.component.ts
+++ src/app/components/view-mecanico-auxiliar/view-mecanico-auxiliar.component.ts
@@ -3,9 +3,9 @@ import { ViewMecanicoAuxiliar } from 'src/app/entities/viewMecanicoAuxiliar';
 import { ViewMecanicoAuxService } from 'src/app/services/view-mecanico-aux.service';
 
 @Component({
-  selector: 'app-view-mecanico-aux',
-  templateUrl: './view-mecanico-aux.component.html',
-  styleUrls: ['./view-mecanico-aux.component.css']
+  selector: 'app-view-mecanico-auxiliar',
+  templateUrl: './view-mecanico-auxiliar.component.html',
+  styleUrls: ['./view-mecanico-auxiliar.component.css']
 })
 export class ViewMecanicoAuxiliarComponent implements OnInit {
   mecanicosAuxiliares:ViewMecanicoAuxiliar[] =[]
